4c5378154cf61126ba273305af3ddcaf074ee7ec
Seth Kinney <sethkinney6@gmail.com>
2021-12-03 11:20:12 -0600

    Big update.
    Wrote unit tests for the get and post methods on the user management page. The get tests mainly ensure that we are retrieving the correct user information from the database, and then displaying the correct information on the page itself. The post tests ensure that a new account is created, with user input being stored correctly. It also ensures that the user is added to the manager group and that the new user is display. Tests also ensure that the system works with multiple users and a variety of odd inputs.
    
    The get method for the user page is relatively simple, it retrieves objects from the database and stores them based on group names. TAs are stored in a TA object, etc. These are then sent to the template, where they are displayed on the webpage.
    
    The post method is slightly more complex. It retrieves the entered username, email, name and password from the form. It then searches the user database for a user with an identical username. If it finds one, no account is created. On the other hand, if it is unable to find one, then a new account is created, with the information from the form used to fill in fields. The new user is then added to the manager group and saved. Then, the accountmanagement page is reloaded so that the new user is displayed.
    
    User management has had python code added to it to display current users. The code loops through the list of TAs, Instructors and administrators sent to the template. Other fields were added such as password and name to create a complete user object. I also altered the name attributes of some elements so that they are compatible with the get and post methods.
    
    Course management template has been updated with early python code that indicates how the page will work. Forms were also added to handle adding sections and courses.

--- TAScheduler/views.py
+++ TAScheduler/views.py
@@ -1,22 +1,39 @@
+from hashlib import sha256
+
 from django.shortcuts import render, redirect
 from django.views import View
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User, Group
 
 class Login(View):
 
     def get(self, request):
-        request.session.clear()
+        logout(request)
+        print(Group.objects.all())
         return render(request, "login.html")
 
     def post(self, request):
-        pass
+        user=request.POST["name"]
+        password=request.POST["password"]
+
+
+
+        userobject=authenticate(request, username=user, password=password)
+
+
+        if not(userobject == None):
+            login(request, userobject)
+            return redirect("/")
+
+        else:
+            return render(request, "login.html")
 
 
 
 class CourseManagement(View):
 
     def get(self, request):
-        return render(request, "courseManagement.html")
+        return render(request, "coursemanagement.html")
 
     def post(self, request):
         pass
@@ -25,14 +42,47 @@ class CourseManagement(View):
 class AccountManagement(View):
 
     def get(self, request):
-        return render(request, "accountManagement.html")
+
+
+        TA=User.objects.filter(groups__name='ta')
+        Instructor=User.objects.filter(groups__name='instructor')
+        Admin=User.objects.filter(groups__name='manager')
+
+
+
+        return render(request, "usermanagement.html", {"TA":TA, "Instructor":Instructor, "Admin":Admin})
 
     def post(self, request):
-        pass
+
+
+
+
+        user=request.POST["username"]
+        email=request.POST["email"]
+        name=request.POST["name"]
+        password=request.POST["password"]
+
+        userthere=User.objects.filter(username=user)
+        groups=Group.objects.filter(name="manager")
+        groupthing=groups[0]
+
+        if(len(userthere)==0):
+
+            newuser=User.objects.create_user(username=user, email=email, first_name=name, password=password)
+
+            newuser.groups.add(groupthing)
+            newuser.save()
+
+
+        return redirect("/accountmanagement/")
+
+
 
 class Home(View):
+
     def get(self, request):
-        return render(request, "home.html")
+        return render(request, "index.html")
+
 
     def post(self, request):
         pass
@@ -41,7 +91,6 @@ class Profile(View):
 
     def get(self, request):
         return render(request, "profile.html")
-
 
 
     def post(self, request):
