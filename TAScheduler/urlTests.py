e66e7ab52fb8624634b768b8510260cb4bdc885b
Justin Gates <gatesjm@uwm.edu>
2021-12-06 20:12:14 -0600

    Updates unit tests to utilize Django authentication functions.

--- TAScheduler/urlTests.py
+++ TAScheduler/urlTests.py
@@ -7,16 +7,24 @@ from .models import Profile
 class TestUrls(TestCase):
     def setUp(self):
         self.client=Client()
+        
+        self.User1 = User.objects.create_user(username="New Guy", password="testing", email="sethkinney6@gmail.com")
+        self.User1.save()
+        Profile1 = Profile(user=self.User1, address="9999", phone="111-111-1111", alt_email="alt@gmail.com")
+        Profile1.save()
 
     def test_loginTemplates(self):
+        self.client.force_login(self.User1)
         r = self.client.get("/login/")
         self.assertEqual(r.templates[0].name, "login.html")
 
     def test_courseCreationTemplates(self):
+        self.client.force_login(self.User1)
         r = self.client.get("/coursemanagement/")
         self.assertEqual(r.templates[0].name, "coursemanagement.html")
 
     def test_accountCreationTemplates(self):
+        self.client.force_login(self.User1)
         r = self.client.get("/accountmanagement/")
         self.assertEqual(r.templates[0].name,  "usermanagement.html")
 
@@ -25,45 +33,30 @@ class TestUrls(TestCase):
         self.assertEqual(r.context.request.path, "/login/")
 
     def test_courseCreationPath(self):
+        self.client.force_login(self.User1)
         r=self.client.get("/coursemanagement/")
         self.assertEqual(r.context.request.path, "/coursemanagement/")
 
     def test_accountCreationPath(self):
+        self.client.force_login(self.User1)
         r=self.client.get("/accountmanagement/")
         self.assertEqual(r.context.request.path, "/accountmanagement/")
 
-
     def test_homeTemplate(self):
-        User1=User.objects.create_user(username="seth", password="bruh")
-        User1.save()
-        session=self.client.session
-        session["_auth_user_id"]=User1.id
-        session.save()
+        self.client.force_login(self.User1)
         r=self.client.get("/")
         self.assertEqual(r.templates[0].name, "index.html")
 
     def test_profileTemplate(self):
-        Users = User.objects.create_user(username="New Guy", password="testing", email="sethkinney6@gmail.com")
-        Users.save()
-        Profile1 = Profile(user=Users, address="9999", phone="111-111-1111", alt_email="alt@gmail.com")
-        Profile1.save()
-        mysession = self.client.session
-        mysession['_auth_user_id'] = Users.id
-        mysession.save()
+        self.client.force_login(self.User1)
         r=self.client.get("/profile/")
         self.assertEqual(r.templates[0].name, "profile.html")
 
     def test_homePath(self):
-        r=self.client.get("/")
-        self.assertRedirects("/login/")
+        r=self.client.get("/", follow=True)
+        self.assertRedirects(r, "/login/?next=/")
 
     def test_profilePath(self):
-        Users=User.objects.create_user(username="New Guy", password="testing", email="sethkinney6@gmail.com")
-        Users.save()
-        Profile1=Profile(user=Users, address="9999", phone="111-111-1111", alt_email="alt@gmail.com")
-        Profile1.save()
-        mysession=self.client.session
-        mysession['_auth_user_id']=Users.id
-        mysession.save()
+        self.client.force_login(self.User1)
         r=self.client.get("/profile/")
         self.assertEqual(r.context.request.path, "/profile/")
