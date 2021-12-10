2fcabc38df9c95c7c3852e6352da86aab53e786c
SOUNDWAVE\Raphael Azcueta <rrazcueta@gmail.com>
2021-12-10 08:43:11 -0600

    - changed usermanagement.html name requirement to be length 1 again - adapted testsessionlab's logintests, but realizing login as an object is kind of much.... idk how to do this

--- TAScheduler/unitTests_Login.py
+++
@@ -1,9 +0,0 @@
-from login import Login
-import unittest
-
-class TestInit(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
-if __name__ == '__main__':
-    unittest.main()
