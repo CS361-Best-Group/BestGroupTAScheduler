7b3faf7fa2bc703ab3150d91097e2355a623fc24
SOUNDWAVE\Raphael Azcueta <rrazcueta@gmail.com>
2021-12-10 08:27:21 -0600

    tweak

--- TAScheduler/unitTests_Login.py
+++ TAScheduler/unitTests_Login.py
@@ -1,11 +1,9 @@
-from login import Fraction
+from login import Login
 import unittest
 
-
-class MyTestCase(unittest.TestCase):
+class TestInit(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
-
 
 if __name__ == '__main__':
     unittest.main()
