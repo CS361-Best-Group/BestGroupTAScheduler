42739028a0a523df0b3143d611daf8614a716805
Leeann Collins <leeanncollins44@gmail.com>
2021-12-17 15:35:56 -0600

    Started working on the methods for determineForm, createUser, and deleteUser.

--- TAScheduler/views.py
+++ TAScheduler/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import User, Group
 
 from TAScheduler.models import Course, Section
 
+
 class Login(View):
 
     def get(self, request):
@@ -17,15 +18,12 @@ class Login(View):
 
     def post(self, request):
         print("In post")
-        user=request.POST["name"]
-        password=request.POST["password"]
+        user = request.POST["name"]
+        password = request.POST["password"]
 
+        userobject = authenticate(request, username=user, password=password)
 
-
-        userobject=authenticate(request, username=user, password=password)
-
-
-        if not(userobject == None):
+        if not (userobject == None):
             print("Login")
             login(request, userobject)
             return redirect("/")
@@ -37,98 +35,105 @@ class Login(View):
 class CourseManagement(LoginRequiredMixin, View):
     def get(self, request):
 
+        Courses = Course.objects.all()
+        Sections = Section.objects.all()
 
-
-        Courses=Course.objects.all()
-        Sections=Section.objects.all()
-
-        return render(request, "coursemanagement.html", {"Courses":Courses, "Sections":Sections})
-
-
+        return render(request, "coursemanagement.html", {"Courses": Courses, "Sections": Sections})
 
     def post(self, request):
 
-
-
-        if("coursename" in request.POST.keys() and "coursedescription" in request.POST.keys() and request.POST["coursename"] != "" and request.POST["coursedescription"] != ""):
+        if ("coursename" in request.POST.keys() and "coursedescription" in request.POST.keys() and request.POST[
+            "coursename"] != "" and request.POST["coursedescription"] != ""):
 
             coursecreationName = request.POST["coursename"]
 
-            if(len(Course.objects.filter(name=coursecreationName))==0):
+            if (len(Course.objects.filter(name=coursecreationName)) == 0):
                 coursecreationDescription = request.POST["coursedescription"]
-                newcourse=Course(name=coursecreationName, description=coursecreationDescription)
+                newcourse = Course(name=coursecreationName, description=coursecreationDescription)
 
                 newcourse.save()
                 newcourse.users.set([])
                 newcourse.save()
 
         elif ("course" in request.POST.keys()):
-            
+
             sectioncreationcourse = request.POST["course"]
-            targetcourse=Course.objects.filter(name=sectioncreationcourse)[0]
+            targetcourse = Course.objects.filter(name=sectioncreationcourse)[0]
 
-            existingsections=str(len(Section.objects.filter(course=targetcourse))+1)
-            newsectionname=targetcourse.name+"-"
+            existingsections = str(len(Section.objects.filter(course=targetcourse)) + 1)
+            newsectionname = targetcourse.name + "-"
 
-            x=0
-            while x+len(existingsections)<3:
-                newsectionname=newsectionname+"0"
-                x=x+1
-            newsectionname=newsectionname+existingsections
+            x = 0
+            while x + len(existingsections) < 3:
+                newsectionname = newsectionname + "0"
+                x = x + 1
+            newsectionname = newsectionname + existingsections
 
-
-            newsection=Section(name=newsectionname, course=targetcourse)
+            newsection = Section(name=newsectionname, course=targetcourse)
             newsection.save()
             newsection.users.set([])
-#            newsection.course=targetcourse
+            #            newsection.course=targetcourse
             newsection.save()
 
         return redirect("/coursemanagement/")
 
-
-
-    #how can I handle multiple forms on the same page?
-
+    # how can I handle multiple forms on the same page?
 
 
 class AccountManagement(LoginRequiredMixin, View):
     def get(self, request):
+        # Nothing will be mapped course fields if post is from a section creation form submission
 
-        #Nothing will be mapped course fields if post is from a section creation form submission
+        TA = User.objects.filter(groups__name='ta')
+        Instructor = User.objects.filter(groups__name='instructor')
+        Admin = User.objects.filter(groups__name='manager')
+        Profiles = Profile.objects.all()
 
-
-        TA=User.objects.filter(groups__name='ta')
-        Instructor=User.objects.filter(groups__name='instructor')
-        Admin=User.objects.filter(groups__name='manager')
-        Profiles=Profile.objects.all()
-
-
-
-        return render(request, "usermanagement.html", {"TA":TA, "Instructor":Instructor, "Admin":Admin, "Profiles":Profiles})
+        return render(request, "usermanagement.html",
+                      {"TA": TA, "Instructor": Instructor, "Admin": Admin, "Profiles": Profiles})
 
     def post(self, request):
-        if all([(field in request.POST) and (request.POST[field] != '') for field in ['username', 'email', 'name', 'password']]):
-            user=request.POST["username"]
-            email=request.POST["email"]
-            name=request.POST["name"]
-            password=request.POST["password"]
-            address= request.POST.get("address", "")
-            phone=request.POST.get("phone", "")
-            altemail=request.POST.get("altemail", "")
-            userthere=User.objects.filter(username=user)
-            groups=Group.objects.filter(name="manager")
-            groupthing=groups[0]
+        if all([(field in request.POST) and (request.POST[field] != '') for field in
+                ['username', 'email', 'name', 'password']]):
+            user = request.POST["username"]
+            email = request.POST["email"]
+            name = request.POST["name"]
+            password = request.POST["password"]
+            address = request.POST.get("address", "")
+            phone = request.POST.get("phone", "")
+            altemail = request.POST.get("altemail", "")
+            userthere = User.objects.filter(username=user)
+            groups = Group.objects.filter(name="manager")
+            groupthing = groups[0]
 
-            if(len(userthere)==0):
-
-                newuser=User.objects.create_user(username=user, email=email, first_name=name, password=password)
+            if (len(userthere) == 0):
+                newuser = User.objects.create_user(username=user, email=email, first_name=name, password=password)
 
                 newuser.groups.add(groupthing)
                 newuser.save()
-                newProfile=Profile(user=newuser, address=address, phone=phone, alt_email=altemail)
+                newProfile = Profile(user=newuser, address=address, phone=phone, alt_email=altemail)
                 newProfile.save()
-                
+
         return redirect("/accountmanagement/")
+
+    def determineForm(self, form):
+        # if not createUser or deleteUser then ValueError
+        if ("username" not in form.keys()):
+            pass
+        # if all forms filled => createUser
+        elif ("username" in form.keys() and "email" in form.keys() and "name" in form.keys() and "password" in form.keys()
+                and "address" in form.keys() and "phone" in form.keys() and "altemail" in form.keys()
+                and "groups" in form.keys()):
+            AccountManagement.createUser(self, form)
+        # if only username filled => deleteUser
+        else:
+            AccountManagement.deleteUser(self, form)
+
+    def createUser(self, form):
+        pass
+
+    def deleteUser(self, form):
+        pass
 
 
 class Home(LoginRequiredMixin, View):
@@ -138,44 +143,47 @@ class Home(LoginRequiredMixin, View):
     def post(self, request):
         pass
 
+
 class ProfilePage(LoginRequiredMixin, View):
     def get(self, request):
 
-        CurrentUserID=request.session["_auth_user_id"]
+        CurrentUserID = request.session["_auth_user_id"]
 
-        CurrentUser=User.objects.filter(id=CurrentUserID)[0]
+        CurrentUser = User.objects.filter(id=CurrentUserID)[0]
 
-        CurrentProfile=Profile.objects.filter(user=CurrentUser)[0]
+        CurrentProfile = Profile.objects.filter(user=CurrentUser)[0]
 
-        return render(request, "profile.html", {"email":CurrentUser.email, "firstname":CurrentUser.first_name, "email":CurrentUser.email, "username":CurrentUser.username, "address":CurrentProfile.address, "phone":CurrentProfile.phone, "altemail":CurrentProfile.alt_email})
+        return render(request, "profile.html",
+                      {"email": CurrentUser.email, "firstname": CurrentUser.first_name, "email": CurrentUser.email,
+                       "username": CurrentUser.username, "address": CurrentProfile.address,
+                       "phone": CurrentProfile.phone, "altemail": CurrentProfile.alt_email})
 
     def post(self, request):
-        newname=request.POST["name"]
-        newusername=request.POST["username"]
-        newphone=request.POST["phone"]
-        newaddress=request.POST["address"]
+        newname = request.POST["name"]
+        newusername = request.POST["username"]
+        newphone = request.POST["phone"]
+        newaddress = request.POST["address"]
         newemail = request.POST["email"]
-        newaltemail=request.POST["altemail"]
+        newaltemail = request.POST["altemail"]
 
-        currentuser=User.objects.filter(id=request.session["_auth_user_id"])[0]
-        currentprofile=Profile.objects.filter(user=currentuser)[0]
+        currentuser = User.objects.filter(id=request.session["_auth_user_id"])[0]
+        currentprofile = Profile.objects.filter(user=currentuser)[0]
 
+        if (newname != ""):
+            currentuser.first_name = newname
 
-        if(newname!=""):
-            currentuser.first_name=newname
+        if (newusername != "" and len(User.objects.filter(username=newusername)) == 0):
+            currentuser.username = newusername
 
-        if(newusername!="" and len(User.objects.filter(username=newusername))==0):
-            currentuser.username=newusername
-
-        if(newphone!=""):
-            currentprofile.phone=newphone
-        if(newaddress!=""):
+        if (newphone != ""):
+            currentprofile.phone = newphone
+        if (newaddress != ""):
             print("inside if statement")
-            currentprofile.address=newaddress
-        if(newemail!=""):
-            currentuser.email=newemail
-        if (newaltemail!=""):
-            currentprofile.alt_email=newaltemail
+            currentprofile.address = newaddress
+        if (newemail != ""):
+            currentuser.email = newemail
+        if (newaltemail != ""):
+            currentprofile.alt_email = newaltemail
 
         currentuser.save()
         currentprofile.save()
