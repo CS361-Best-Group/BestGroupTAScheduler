c04ee4e604c9fda8daf8699e786b6f1957543b67
Leeann Collins <leeanncollins44@gmail.com>
2021-12-17 15:16:09 -0600

    Added new folder for unittests, changed an import, changed arguments passed into methods

--- TAScheduler/UnitTests/unittests_user_post.py
+++ TAScheduler/UnitTests/unittests_user_post.py
@@ -1,9 +1,9 @@
-import unittest
+from django.test import TestCase
 from django.contrib.auth.models import User
 from TAScheduler.views import AccountManagement
 
 
-class TestCreateUser(unittest.TestCase):
+class TestCreateUser(TestCase):
     def setUp(self):
         self.userA = {"username": "User",
                       "email": "user@uwm.edu",
@@ -39,28 +39,28 @@ class TestCreateUser(unittest.TestCase):
                       "groups": "ta"}
 
     def test_Init_Admin(self):
-        AccountManagement.createUser(self, self.userA)
-        # check that gooduser was added to database
+        AccountManagement.createUser(AccountManagement, self.userA)
+        # check that userA was added to database
         self.assertTrue(len(User.objects.filter(username="User")) == 1)
 
     def tests_Init_Instructor(self):
-        AccountManagement.createUser(self, self.userI)
+        AccountManagement.createUser(AccountManagement, self.userI)
         self.assertTrue(len(User.objects.filter(username="UserI")) == 1)
 
     def tests_Init_TA(self):
-        AccountManagement.createUser(self, self.userT)
+        AccountManagement.createUser(AccountManagement, self.userT)
         self.assertTrue(len(User.objects.filter(username="UserT")) == 1)
 
     # throws error if trying to create a duplicate username --> user
     def test_DupUser(self):
-        AccountManagement.createUser(self, self.userA)
+        AccountManagement.createUser(AccountManagement, self.userA)
         # Throw error as userA2 uses the same username as userA
         # Do not add userA2
         with self.assertRaises(ValueError, msg="Duplicate Username"):
-            AccountManagement.createUser(self, self.userA2)
+            AccountManagement.createUser(AccountManagement, self.userA2)
 
 
-class TestDeleteUser(unittest.TestCase):
+class TestDeleteUser(TestCase):
     def setUp(self):
         self.userA = {"username": "UserAdm",
                       "email": "user@uwm.edu",
@@ -89,29 +89,29 @@ class TestDeleteUser(unittest.TestCase):
 
     def test_delete_Admin(self):
         # Creating a user to test delete
-        AccountManagement.createUser(self, self.userA)
+        AccountManagement.createUser(AccountManagement, self.userA)
         # delete user
-        AccountManagement.deleteUser(self, self.userA["username"])
+        AccountManagement.deleteUser(AccountManagement, self.userA["username"])
         # check that user was fully deleted
         self.assertTrue(len(User.objects.filter(username="UserAdm")) == 0)
 
     def test_delete_Instructor(self):
-        AccountManagement.createUser(self, self.userI)
-        AccountManagement.deleteUser(self, self.userI["username"])
+        AccountManagement.createUser(AccountManagement, self.userI)
+        AccountManagement.deleteUser(AccountManagement, self.userI["username"])
         self.assertTrue(len(User.objects.filter(username="UserIns")) == 0)
 
     def test_delete_TA(self):
-        AccountManagement.createUser(self, self.userT)
-        AccountManagement.deleteUser(self, self.userT["username"])
+        AccountManagement.createUser(AccountManagement, self.userT)
+        AccountManagement.deleteUser(AccountManagement, self.userT["username"])
         self.assertTrue(len(User.objects.filter(username="UserTA")) == 0)
 
     # throws error if trying to delete a user that does not exist
     def test_delete_NoUser(self):
         with self.assertRaises(KeyError, msg="No user to delete"):
-            AccountManagement.deleteUser(self, self.userA["username"])
+            AccountManagement.deleteUser(AccountManagement, self.userA["username"])
 
 
-class TestDetermineForm(unittest.TestCase):
+class TestDetermineForm(TestCase):
     def setUp(self):
         self.create = {"username": "UserN",
                        "email": "usern@uwm.edu",
@@ -120,22 +120,22 @@ class TestDetermineForm(unittest.TestCase):
                        "address": "123 This St",
                        "phone": "1234567899",
                        "altemail": "usern@gmail.com",
-                       "group__name": "admin"}
+                       "groups": "admin"}
         self.delete = {"username": "UserN"}
         self.other = {"name": "NewUser"}
         self.empty = {""}
 
     def test_createForm(self):
-        AccountManagement.determineForm(self, self.create)
+        AccountManagement.determineForm(AccountManagement, self.create)
         self.assertTrue(len(User.objects.filter(username="UserN")) == 1)
 
     def test_deleteForm(self):
-        AccountManagement.determineForm(self, self.create)
-        AccountManagement.determineForm(self, self.delete)
+        AccountManagement.determineForm(AccountManagement, self.create)
+        AccountManagement.determineForm(AccountManagement, self.delete)
         self.assertTrue(len(User.objects.filter(username="UserN")) == 0)
 
     # throws error if it gets passed a form other than whats expected (create or delete)
     def test_otherForm(self):
         with self.assertRaises(ValueError, msg="Didn't receive 'deleteUser' xor 'createUser'"):
-            AccountManagement.determineForm(self, self.other)
+            AccountManagement.determineForm(AccountManagement, self.other)
 
