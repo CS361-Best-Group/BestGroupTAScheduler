a5a6d949ff7e9f902d008a0224dd5f84276cdace
Seth Kinney <sethkinney6@gmail.com>
2021-12-18 13:10:09 -0600

    Mostly small changes/bugfixes. I update determinerole.py to check make sure that the user is actually a part of the TA group before returning ta, otherwise it will display superusers as TAs on the User page.
    In unitests.py, I updated my unit tests for the determinerole.py to match the detemrinerole implementation.
    
    For unitests_profilypage, I changed the tests so that they are passing and run correctly. The classes had to have the django TestCase passed in, some method calls needed an extra argument, and I also changed the second test class to test TA profile correctly.
    
    Url tests had to import group in order to work.
    
    In Usermanagement.html, I deleted the assign user to course/section buttons.
    
    In views, I deleted my implementation of determinerole.py and reworked accountManagement.get to use the new implementation.

--- TAScheduler/AcceptanceTests/urlTests.py
+++ TAScheduler/AcceptanceTests/urlTests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.test import Client
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from TAScheduler.models import Profile
 
 
